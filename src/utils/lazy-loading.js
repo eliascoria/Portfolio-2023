67d8d73e1b069a2f807cf818067264c915966c9c
eliascoria <eliascoria015@gmail.com>
2023-09-03 23:55:36 -0300

    update images

--- src/utils/lazy-loading.js
+++ src/utils/lazy-loading.js
@@ -1,23 +1,22 @@
 const lazyLoading = () => {
+  
+  const lazyImgs = document.querySelectorAll('.lazy');
 
-const lazyImgs = document.querySelectorAll('.lazy');
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let img = entry.target;
+        img.src = img.dataset.src;
+        img.classList.remove('loading');
+        img.classList.add('loaded');
+        observer.unobserve(img);
+      }
+    });
+  });
 
-const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      let img = entry.target;
-      img.src = img.dataset.src;
-      img.classList.remove('loading');
-      img.classList.add('loaded');
-      observer.unobserve(img);
-      // console.log(img)
-    }
-  })
-});
-
-lazyImgs.forEach(img => {
-  observer.observe(img);
-});
+  lazyImgs.forEach(img => {
+    observer.observe(img);
+  });
 };
 
 export default lazyLoading;
